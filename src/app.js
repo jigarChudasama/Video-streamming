0e233f5e03fb56253ed0ed039f77149e368aa9d5
jigarChudasama <jigarc446@gmail.com>
2025-08-22 18:38:10 +0530

    work on utils

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import exprass from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = exprass()
+
+app.use(cors({
+    origin : process.env.CORD_ORIGEN,
+    credentials : true
+}))
+app.use(exprass.json({limit : '16kb'}))
+app.use(exprass.urlencoded({extended : true ,limit : '16kb'}))
+app.use(exprass.static('public'))
+app.use(cookieParser())
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 dotenv.config({
     path : './env'
 })
 
-connectDB() 
+
+connectDB()
+.then( ()=>{
+    app.listen( process.env.PORT || 8000 , ()=>{
+        console.log(`server is running in ${process.env.PORT}`)
+    } )
+    app.on('error', (error) => {
+                console.log( "connection error in app.on" , error)
+                throw error
+            })
+} )
+.catch( (err)=>{
+    console.log(" Mongo DB connection error " , err )
+} )
 
 
 
