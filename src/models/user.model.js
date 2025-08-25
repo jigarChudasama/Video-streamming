45f5bfb839c90a5dfed7d7ab87ccccd4a4e704e2
jigarChudasama <jigarc446@gmail.com>
2025-08-25 15:50:25 +0530

    add jwt token system

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,46 +4,46 @@ import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
     {
-        username : {
-            type : String,
-            required : true,
-            unique : true,
-            trim : true ,
-            index : true,
-            lowercase : true
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true,
+            lowercase: true
         },
-        email : {
-            type : String,
-            required : true,
-            unique : true,
-            trim : true ,
-            lowercase : true
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
         },
-        fullname : {
-            type : String,
-            required : true,
-            trim : true ,
-            index : true,
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
         },
-        avatar : {
-            type : String,
-            required : true,
+        avatar: {
+            type: String,
+            required: true,
         },
-        coverImage : {
-            type : String,
+        coverImage: {
+            type: String,
         },
-        watchHistory : [
+        watchHistory: [
             {
                 type: Schema.Types.ObjectId,
-                ref : 'Video'
+                ref: 'Video'
             }
         ],
-        password : {
-            type : String,
-            required : [true , "password is required"]
+        password: {
+            type: String,
+            required: [true, "password is required"]
         },
-        refrashTokan : {
-            type : String
+        refrashTokan: {
+            type: String
         }
     },
     {
@@ -51,16 +51,43 @@ const userSchema = new Schema(
     }
 )
 
-userSchema.pre('save' , function () {
+userSchema.pre('save', function () {
 
-    if(!this.isModified('password')) return next() 
+    if (!this.isModified('password')) return next()
 
-    this.password = bcrypt.hash(this.password , 10)
+    this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password , this.password )
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+return jwt.sign (
+    {
+        _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : REFRESH_TOKEN_EXPIRY
+    }
+)
 }
 
 export const User = mongoose.model("User", userSchema)
