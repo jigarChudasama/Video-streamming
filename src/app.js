67af8fa73be5cb7c2658b3127be6d93e5198606a
jigarChudasama <jigarc446@gmail.com>
2025-08-26 15:00:12 +0530

    the setup complete here

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(exprass.urlencoded({extended : true ,limit : '16kb'}))
 app.use(exprass.static('public'))
 app.use(cookieParser())
 
+// routes import
+
+import UserRoutes from './routes/user.routes.js'
+
+// route declaration 
+
+app.use( '/api/v1/users' , UserRoutes )
+// http://localhost:8000/api/v1/users/register
 export { app }  
