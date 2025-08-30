b78786cfa583549d1a6a930b98260cf15b1fc828
jigarChudasama <jigarc446@gmail.com>
2025-08-30 11:58:40 +0530

    add watch history and new routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from 'jsonwebtoken'
 import { deleteFromCloudinary } from "../utils/OldImageDelete.js";
+import mongoose from "mongoose";
 
 // =================== Generate Tokens ===================
 const generateAccessAndRefereshTokens = async (userId) => {
@@ -278,7 +279,7 @@ const updateUserDetail = asyncHandler(async (req, res) => {
 })
 
 // ================== change avatar  ===================
-const changeAvatar = asyncHandler(async (req, res) => {
+const updateUserAvatar = asyncHandler(async (req, res) => {
     const localAvatarPath = req.file?.path
 
     if (!localAvatarPath) {
@@ -439,6 +440,63 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         )
 })
 
+// =================== get watch history using an aggregate pipeline (mingoDB) ===================
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "watch history fetach successfully "
+        )
+    )
+
+})
+
 // =================== Exports ===================
 export {
     registerUser,
@@ -448,7 +506,8 @@ export {
     ChnagePassword,
     getCurrntUser,
     updateUserDetail,
-    changeAvatar,
+    updateUserAvatar,
     updateCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { ChnagePassword, getCurrntUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateCoverImage, updateUserAvatar, updateUserDetail } from "../controllers/user.controller.js";
 import { upload } from '../middlewares/multer.middleware.js'
 import { varifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -23,5 +23,19 @@ router.route('/login').post( loginUser )
 router.route('/logout').post( varifyJWT , logoutUser)
 
 router.route('/refesh-token').post(refreshAccessToken)
+
+router.route('/Chnage-password').post(varifyJWT , ChnagePassword)
+
+router.route('/current-user').get(varifyJWT , getCurrntUser)
+
+router.route('/update-account').patch(varifyJWT , updateUserDetail)
+
+router.route('/avatar').patch(varifyJWT , upload.single("avatar") , updateUserAvatar)
+
+router.route('/coverImage').patch(varifyJWT , upload.single("coverImage") , updateCoverImage)
+
+router.route('/c/:username').get(varifyJWT , getUserChannelProfile)
+
+router.route('/watch-history').get(varifyJWT , getWatchHistory )
 
 export default router   
